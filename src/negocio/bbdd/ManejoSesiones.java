bee04ee50e63ac502bfe5e1f5847f98874dcc5b1
Hugo de Benito <hdeben01@ucm.es>
2025-03-08 12:11:31 +0100

    creo clases de InicioSesion

---
+++ src/negocio/bbdd/ManejoSesiones.java
@@ -0,0 +1,18 @@
+package negocio.bbdd;
+
+public class ManejoSesiones {
+
+	public ManejoSesiones() {
+		
+	}
+	
+	
+	public int InicioSesion(String usuario, String contrasenya) {
+		//TODO Checkear si existe el usuario y la contraseña tiene mas de X caracteres + otros criterios
+		//TODO Crear el usuario y contraseña
+		//TODO Crearlo en la base de datos
+		return 0;
+		
+	}
+	
+}

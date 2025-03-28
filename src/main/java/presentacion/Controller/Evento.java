f2e202bd27c2ec4a59d0f389bad57439b296d5ed
abrahaamg <ibgarces@ucm.es>
2025-03-28 17:14:59 +0100

    evento actualizado de nuevo

--- src/main/java/presentacion/Controller/Evento.java
+++ src/main/java/presentacion/Controller/Evento.java
@@ -3,7 +3,7 @@ package presentacion.Controller;
 public enum Evento {
 
 
-        // ----------------- EVENTOS COMUNES -----------------
+    // ----------------- EVENTOS COMUNES -----------------
 
     ERROR_CADENA_NO_ALFABETICA, // La cadena no contiene SOLO caracteres alfabeticos
     ERROR_TIPO_DOCUMENTO_INVALIDO, // Tipo de documento no valido
@@ -22,31 +22,33 @@ public enum Evento {
     // Inicio de sesion exitoso
     INICIO_SESION_OK,
 
-    // ----------------- CREACION DE TARJETA DE DEBITO -----------------
+ // ----------------- CREACION DE TARJETA DE DEBITO -----------------
     GUI_CREAR_TARJETA_DEBITO, // Evento para abrir la vista de creacion de tarjeta
 
     CREAR_TARJETA_DEBITO, // Evento para ejecutar la creacion de la tarjeta
 
     // Errores en la creacion de tarjeta de debito
-    CREAR_TARJETA_ERROR_TARJETA_NULL, //La tarjeta no es valida (es null)
-    CREAR_TARJETA_ERROR_DATOS_INCOMPLETOS, // Faltan datos obligatorios
-    CREAR_TARJETA_ERROR_DATOS_NULOS, //Datos nulos
+    CREAR_TARJETA_ERROR_TARJETA_NULL,
+    CREAR_TARJETA_ERROR_DATOS_INCOMPLETOS,
+    CREAR_TARJETA_ERROR_DATOS_NULOS,
     CREAR_TARJETA_ERROR_CUENTA_INEXISTENTE, // La cuenta IBAN no existe en la base de datos
     CREAR_TARJETA_ERROR_DB, // Error al insertar la tarjeta en la base de datos
     
     // Tarjeta creada con exito
-    CREAR_TARJETA_OK,    
+    CREAR_TARJETA_OK,
+    
+    
  // ----------------- CREACION DE CUENTA BANCARIA -----------------
     GUI_CREAR_CUENTA_BANCARIA, // Evento para abrir la vista de creacion de tarjeta
 
-    CREAR_CUENTA_BANCARIA, // Evento para ejecutar la creacion de la tarjeta
+    CREAR_CUENTA_BANCARIA, // Evento para ejecutar la creacion de la cuenta
 
-    // Errores en la creacion de tarjeta de debito
-    CREAR_CUENTA_BANCARIA_ERROR_CUENTA_NULL, //La cuenta no es valida (es null)
-    CREAR_CUENTA_BANCARIA_ERROR_DATOS_INCOMPLETOS, // Faltan datos obligatorios
-    CREAR_CUENTA_BANCARIA_ERROR_DATOS_NULOS, // Datos nulos
+    // Errores en la creacion de cuenta
+    CREAR_CUENTA_BANCARIA_ERROR_CUENTA_NULL,
+    CREAR_CUENTA_BANCARIA_ERROR_DATOS_INCOMPLETOS,
+    CREAR_CUENTA_BANCARIA_ERROR_DATOS_NULOS,
     CREAR_CUENTA_BANCARIA_ERROR_DB, // Error al insertar la cuenta en la base de datos
     
-    // Tarjeta creada con exito
+    // Cuenta creada con exito
     CREAR_CUENTA_BANCARIA_OK;
 }

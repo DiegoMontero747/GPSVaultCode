92afd81cc48f023ce4e7d65e5e592d498008dc78
JuanParedes15903 <juapar02@ucm.es>
2025-03-31 21:36:53 +0200

    Comprobacion tlf y contraseña

--- src/main/java/negocio/ManejoSesiones/SACrearCuentaAdministracionImp.java
+++ src/main/java/negocio/ManejoSesiones/SACrearCuentaAdministracionImp.java
@@ -11,85 +11,88 @@ import integracion.bbdd.MongoDBManager;
 import negocio.Factory.ResultContext;
 import presentacion.Controller.Evento;
 
-public class SACrearCuentaAdministracionImp implements SACrearCuentaAdministracion{
+public class SACrearCuentaAdministracionImp implements SACrearCuentaAdministracion {
 
 	private MongoDBManager db;
-	
+
 	public SACrearCuentaAdministracionImp() {
 		db = MongoDBManager.getInstance();
 	}
-	
+
 	public SACrearCuentaAdministracionImp(MongoDBManager db) {
 		this.db = db;
 	}
-	
+
 	@Override
 	public ResultContext crearCuenta(TCrearCuentaAdm data) {
 
-		
-		if(data.nullData()) {
-			
-			return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_DATOS_NULOS,data);
-		}
-		
-		if (data.datosVacios()) {
-			return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_DATOS_VACIOS,data);
-		}
-		
-	
-		
-		if(validarDNI(data.getDni()) || validarNIE(data.getDni())) {
-			List<Document> perfiles = db.readDocument(null,Collections.PERFIL);
+		if (data.nullData()) {
 
-			//iteramos sobre los perfiles buscando si ya hay alguno con ese DNI
-			for(Document doc: perfiles) {
-				if(doc.containsValue(data.getDni())) {
-					return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_DNI_ENCONTRADO,null);
+			return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_DATOS_NULOS, data);
+		}
+
+		if (data.datosVacios()) {
+			return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_DATOS_VACIOS, data);
+		}
+
+		if (validarDNI(data.getDni()) || validarNIE(data.getDni())) {
+			List<Document> perfiles = db.readDocument(null, Collections.PERFIL);
+
+			// iteramos sobre los perfiles buscando si ya hay alguno con ese DNI
+			for (Document doc : perfiles) {
+				if (doc.containsValue(data.getDni())) {
+					return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_DNI_ENCONTRADO, null);
 				}
 			}
-			//comprobamos que el rol introducido es el correcto
+			// comprobamos que el rol introducido es el correcto
+
+			if(validarTLF(data.getTelf()) ) {
+				return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_TEL_INCORRECTO, null);
+			}
 			
-			if(validarROL(data.getRol())) {
+			if (validarPASSWD(data.getConstrasenya())) {
 				Document nuevoperfil = new Document();
-				nuevoperfil.append("nombre",data.getNombre());
-				nuevoperfil.append("apellido",data.getApellido());
-				nuevoperfil.append("rol",data.getRol());
-				nuevoperfil.append("password",data.getConstrasenya());
-				nuevoperfil.append("usuario",data.getUsuario());
-				nuevoperfil.append("dni",data.getDni());
+				nuevoperfil.append("nombre", data.getNombre());
+				nuevoperfil.append("apellido", data.getApellido());
+				nuevoperfil.append("rol", data.getRol());
+				nuevoperfil.append("password", data.getConstrasenya());
+				nuevoperfil.append("usuario", data.getUsuario());
+				nuevoperfil.append("dni", data.getDni());
 				nuevoperfil.append("telefono", data.getTelf());
 				db.insertDocument(Collections.PERFIL, nuevoperfil);
-				//devuelve data para devolver los datos introducidos por si hacen falta
-				return new ResultContext(Evento.CREAR_CUENTA_ADM_EXITO,data);
-				
+				// devuelve data para devolver los datos introducidos por si hacen falta
+				return new ResultContext(Evento.CREAR_CUENTA_ADM_EXITO, data);
+
+			} else {
+				return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_PASSWD_INCORRECTA, null);
 			}
-			else {
-				return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_ROL_INCORRECTO,null);
-			}
-			
+
+		} else {
+			return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_FORMATO_DNI, null);
 		}
-		else {
-			return new ResultContext(Evento.CREAR_CUENTA_ADM_ERROR_FORMATO_DNI,null);
-		}
-		
+
 	}
-	
+
 // Validar DNI: 8 numeros y una letra
 	private boolean validarDNI(String dni) {
-	    String regexDNI = "^[0-9]{8}[A-Za-z]$";
-	    return dni.matches(regexDNI);
+		String regexDNI = "^[0-9]{8}[A-Za-z]$";
+		return dni.matches(regexDNI);
 	}
 
 	// Validar NIE: Letra X/Y/Z, 7 numeros y una letra
 	private boolean validarNIE(String nie) {
-	    String regexNIE = "^[XYZ]\\d{7}[A-Za-z]$";
-	    return nie.matches(regexNIE);
+		String regexNIE = "^[XYZ]\\d{7}[A-Za-z]$";
+		return nie.matches(regexNIE);
 	}
-	
-	// Validar NIE: Letra X/Y/Z, 7 numeros y una letra
-		private boolean validarROL(String rol) {
-			String rolRegex = "(?i)^(servicios centrales|administracion|activo|pasivo)$";
-		    return rol.matches(rolRegex);
-		}
 
+	// Validar TEL:
+	private boolean validarTLF(String tlf) {
+		String tlfRegex = "^[0-9]$";
+		return tlf.matches(tlfRegex);
+	}
+
+	private boolean validarPASSWD(String passwd) {
+		
+		return passwd.length() >= 8;
+	}
 }
